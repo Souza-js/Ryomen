0860df5856311772ef0a82e62152a74b5f8a3434
Souza <souzarodrigo0610@gmail.com>
2024-09-26 22:08:13 -0300

    update nas vars

--- src/types/events/variavel.js
+++ src/types/events/variavel.js
@@ -1,16 +1,16 @@
 module.exports = (client) => {
-client.variables{(
-// ------[ Ryomen - Configuração]------
+    client.variables({
+    // ------[ Ryomen - Configuração]------
 prefix: "r!",
-// ------[ Ryomen - Sistema ]------
-sistemarpg: "0",
-// ------[ Ryomen - atribuíveis ]------
-cc: "0",
-madeira: "0",
-pedra: "0",
-carvão: "0",
-ouro: "0", 
-diamante: "0",
-ferro: "0",
-)};
-};
+    // ------[ Ryomen - Sistema ]------
+    sistemarpg: "0",
+    // ------[ Ryomen - atribuíveis ]------
+    cc: "0",
+    madeira: "0",
+    pedra: "0",
+    carvão: "0",
+    ouro: "0", 
+    diamante: "0",
+    ferro: "0",
+    });
+    };
